60842019eac1acbde9a722a15e4bcc4aa038d2b5
NemesisUA <display.n0ne.01.gmail.com>
2023-02-23 14:08:19 +0200

    fix: improve footer adaptive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="wrapper">
-                <p>Виконано в <a href="https://prometheus.org.ua/" target="_blank" rel="noreferrer"> Pr<span></span>metheus </a> &copy; 2022</p>
+                <p>Виконано в<a href="https://prometheus.org.ua/" target="_blank" rel="noreferrer">Pr<span></span>metheus</a>&copy;2022</p>
             </div>
         </footer>
     )
